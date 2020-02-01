87c87734ef4950523796cbd43b805ae3ab586dee
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2020-02-01 11:48:07 +0100

    Add fbs

---
+++ src/main/python/main.py
@@ -0,0 +1,11 @@
+from fbs_runtime.application_context.PySide2 import ApplicationContext
+from run_it.main_widget import MainWidget
+import sys
+
+if __name__ == '__main__':
+    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+    window = MainWidget()
+    window.resize(1200, 800)
+    window.show()
+    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    sys.exit(exit_code)
